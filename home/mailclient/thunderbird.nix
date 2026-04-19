1982a089a75b75fca7a6d7d7af6250c4df8e0135
jthulhu <jthulhu@posteo.net>
2026-04-19 16:20:48 +0200

    [home/thunderbird]  Changed code to future accept extensions.

--- home/mailclient/thunderbird.nix
+++ home/mailclient/thunderbird.nix
@@ -3,6 +3,13 @@
   programs.thunderbird = {
     enable = true;
     package = pkgs.thunderbird-140;
-    profiles.default.isDefault = true;
+    profiles.default = {
+      isDefault = true;
+      extensions = [];
+      settings = {
+        # Automatically enable add-ons.
+        "extensions.autoDisableScopes" = 0;
+      };
+    };
   };
 }

1cf6edd5e259e5cd42a07202b6bedf41080be4a3
jthulhu <jthulhu@posteo.net>
2026-04-19 20:03:29 +0200

    [home/eww]  Adding eww as a service.

--- home/eww/default.nix
+++ home/eww/default.nix
@@ -1,9 +1,25 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   programs.eww = {
     enable = true;
-    package = pkgs.eww;
     configDir = ./eww.d;
+  };
+
+  systemd.user.services.eww = {
+    Unit = {
+      Description = "";
+      Documentation = "https://elkowar.github.io/eww";
+      PartOf = "graphical-session.target";
+    };
+    Service = {
+      Type = "exec";
+      ExecStart = "${config.programs.eww.package}/bin/eww daemon --no-daemonize";
+      ExecReload = "${config.programs.eww.package}/bin/eww reload";
+      Restart = "on-failure";
+    };
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
   };
 
   services.playerctld.enable = true;

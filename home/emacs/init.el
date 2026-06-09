10b489bde36a385ec6f45c8d09c9733ec90a01f1
jthulhu <jthulhu@posteo.net>
2026-06-09 14:38:30 +0200

    [home/emacs]  Added tons of useful packages, plus some sources.
    Added modes:
    - gcmh
    - apheleia
    - buffer guardian
    - dtrt-indent
    - indent-bars
    - kirigami
    - treesit-fold
    - diff-hl
    - git-modes
    - outline-indent

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -2,6 +2,9 @@
 
 (require 'org)
 
+(delete-file (expand-file-name "settings.el"
+			       user-emacs-directory))
+
 (org-babel-load-file
  (expand-file-name "settings.org"
                    user-emacs-directory))
